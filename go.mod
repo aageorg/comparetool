028c9be3039ef23ce838702c2952ac5b81ab6b9c
Nastasia Georg <nastasia@ndl.ru>
2023-03-16 01:11:36 +0300

    Data processing operations are placed in goroutines

--- go.mod
+++ go.mod
@@ -2,6 +2,6 @@ module compare
 
 go 1.19
 
-require github.com/aageorg/altclient v0.0.0-20230315190732-501442ec2ee4
+require github.com/aageorg/altclient v0.0.0-20230315195255-882b5c7bb2ab
 
 require github.com/hashicorp/go-version v1.6.0 // indirect
--- main.go
+++ main.go
@@ -12,16 +12,15 @@ import (
 type Response [2]Diff
 
 type Diff struct {
-	BranchName string                         `json:"branch"`
+	BranchName string `json:"branch"`
 
-// The map with a missing packages in the branch. Key is the arch name (e.g. "aarch64")
+	// The map with a missing packages in the branch. Key is the arch name (e.g. "aarch64")
 
-	Missing    map[string][]altclient.Package `json:"missing"`
+	Missing map[string][]altclient.Package `json:"missing"`
 
-// The map with the out of date packages in the branch. Key is the arch name (e.g. "aarch64")
+	// The map with the out of date packages in the branch. Key is the arch name (e.g. "aarch64")
 
-	OutOfDate  map[string][]altclient.Package `json:"out_of_date,omitempty"`
-
+	OutOfDate map[string][]altclient.Package `json:"out_of_date,omitempty"`
 }
 
 func main() {
@@ -32,7 +31,7 @@ func main() {
 	}
 	re := regexp.MustCompile(`^[a-zA-Z0-9\_\-]+$`)
 	if !re.Match([]byte(os.Args[1])) || !re.Match([]byte(os.Args[2])) {
-		fmt.Fprintln(os.Stderr, "Invalid branch name. Usage: altcompare branch1 branch2")
+		fmt.Fprintln(os.Stderr, "Invalid branch name. Usage: compare branch1 branch2")
 
 		return
 	}
@@ -63,20 +62,27 @@ func main() {
 		}
 	}()
 	wg.Wait()
-	if branch1 == nil || branch2 == nil {
-		return
-	}
+	wg.Add(2)
+
 	var d1 = Diff{BranchName: os.Args[1], Missing: make(map[string][]altclient.Package)}
-	archs := branch1.GetArchs()
-	for _, a := range archs {
-		d1.Missing[a] = branch2.GetMissing(branch1, a)
-	}
 	var d2 = Diff{BranchName: os.Args[2], Missing: make(map[string][]altclient.Package), OutOfDate: make(map[string][]altclient.Package)}
-	archs = branch2.GetArchs()
-	for _, a := range archs {
-		d2.Missing[a] = branch1.GetMissing(branch2, a)
-		d2.OutOfDate[a] = branch1.GetOutOfDate(branch2, a)
-	}
+
+	go func() {
+		defer wg.Done()
+		archs := branch1.GetArchs()
+		for _, a := range archs {
+			d1.Missing[a] = branch2.GetMissing(branch1, a)
+		}
+	}()
+	go func() {
+		defer wg.Done()
+		archs := branch2.GetArchs()
+		for _, a := range archs {
+			d2.Missing[a] = branch1.GetMissing(branch2, a)
+			d2.OutOfDate[a] = branch1.GetOutOfDate(branch2, a)
+		}
+	}()
+	wg.Wait()
 
 	var r Response
 	r[0] = d1
@@ -84,7 +90,8 @@ func main() {
 
 	js, err := json.Marshal(r)
 	if err != nil {
-		panic(err.Error())
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
 	}
 	fmt.Fprintln(os.Stdout, string(js))
 }
